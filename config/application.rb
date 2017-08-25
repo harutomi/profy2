979950b7998e359622809ea309fc71fdb7c474c2
富永治明 <tominagaharuaki@MacBook-Pro.local>
2017-08-25 18:19:26 +0900

    The standard login function was completed

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module Profy2
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    config.i18n.default_locale = :ja #コメントアウトしている場合は外すだけで良い。
   end
 end
